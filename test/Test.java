e34a8aca9ae13f30269c87bca6c585837231fc0c
emilietexier <45147990+emilietexier@users.noreply.github.com>
2019-12-17 17:46:41 +0100

    created test class

---
+++ test/Test.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args){
+        //We will generate a random character array to be tested by the game verifier
+        char[][] arr = new char[Constants.dimension][Constants.dimension];
+        for(int i = 0; i < Constants.dimension; ++i){
+            for(int j = 0; j < Constants.dimension; ++j){
+                Random randomGenerator = new Random();
+                int randomInt = randomGenerator.nextInt(3); //generates a random int between 0, 1 and 2
+                switch(randomInt) {
+                    case 0: //0 is equivalent to X
+                        arr[i][j] = 'X';
+                        break;
+                    case 1: //1 is equivalent to O
+                        arr[i][j] = 'O';
+                        break;
+                    case 2: //2 is equivalent to a blank
+                        arr[i][j] = ' ';
+                }
+                System.out.print(arr[i][j]);
+                System.out.print('|');
+            }
+            System.out.println("");
+        }Grid grid = new Grid(arr); //declaring our random array as a grid
+        char response = grid.checkState();
+        switch (response) {
+            case 'X':
+                System.out.println("Player X wins !");
+                break;
+            case 'O':
+                System.out.println("Player O wins !");
+                break;
+            case '?':
+                System.out.println("The outcome of the game is still to be determined.");
+                break;
+        }
+
+    }
+
+}
